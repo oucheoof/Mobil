350965ec9ad215b6ba85a98b3e832ccae3cf7b39
emblalundqvist <embla@dotvoid.se>
2023-12-21 17:48:38 +0100

    style

--- components/postit.js
+++ components/postit.js
@@ -1,16 +1,16 @@
-function renderPostit() {
-    const body = document.querySelector("body");
-    const div = document.createElement("div");
-    document.body.appendChild(div);
-    div.id = "postit";
-    div.innerHTML = `
-    <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-    </ul>
-`;
-}
+//function renderPostit() {
+   // const body = document.querySelector("body");
+   // const div = document.createElement("div");
+   // document.body.appendChild(div);
+   // div.id = "postit";
+   // div.innerHTML = `
+   // <ul>
+       // <li>1</li>
+       // <li>2</li>
+       // <li>3</li>
+       // <li>4</li>
+   // </ul>
+//`;
+//}
 
-renderPostit();
+//renderPostit();
